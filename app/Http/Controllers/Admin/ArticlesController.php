e5a4aa7290939a3149d283e799357c888db34628
akim157 <akim157@mail.ru>
2018-11-13 18:47:14 +0300

    18

--- app/Http/Controllers/Admin/ArticlesController.php
+++ app/Http/Controllers/Admin/ArticlesController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Corp\Http\Controllers\Controller;
 use Corp\Repositories\ArticlesRepository;
 use Gate;
+use Corp\Category;
 
 class ArticlesController extends AdminController
 {
@@ -51,7 +52,24 @@ class ArticlesController extends AdminController
      */
     public function create()
     {
-        //
+        if(Gate::denies('save', new \Corp\Article)) {
+            abort(403);
+        }
+
+        $this->title = 'Добавить новый материал';
+        $categories = Category::select(['title', 'alias', 'parent_id', 'id'])->get();
+        $lists = array();
+        foreach($categories as $category){
+            if($category->parent_id == 0) {
+                $lists[$category->title] = array();
+            }
+            else {
+                $lists[$categories->where('id', $category->parent_id)->first()->title][$category->id] = $category->title;
+            }
+        }
+        
+        $this->content = view(env('THEME').'.admin.articles_create_content')->with('categories', $lists)->render();
+        return $this->renderOutput();
     }
 
     /**
